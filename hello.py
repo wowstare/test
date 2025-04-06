a05c337ccb11c5a867091bff2780ae284308bead
Narges alyhare <48632003+wowstare@users.noreply.github.com>
2025-04-06 19:27:25 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,29 @@
+print("Hello, World!")
+name = "John"  # Text (string)
+age = 10       # Number (integer)
+height = 5.5   # Decimal (float)
+
+print(name)
+print(age)
+print(height)
+print(5 + 3)   # Addition
+print(10 - 2)  # Subtraction
+print(4 * 3)   # Multiplication
+print(10 / 2)  # Division
+print(10 % 3)  # Modulus (remainder)
+text = "Python is fun!"
+print(text.upper())    # UPPERCASE
+print(text.lower())    # lowercase
+print(len(text))       # Length of the string
+print(text[0])         # First character
+width = 5
+height = 10
+area = width * height
+print("The area is:", area)
+age = int(input("How old are you? "))
+print("Next year, you will be:", age + 1)
+for i in range(5):  # Repeats 5 times
+    print("Hello!", i)
+fruits.append("orange")  # Add an item
+fruits.remove("banana")  # Remove an item
+print(fruits)
